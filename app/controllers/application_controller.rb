9a5d1a32957e6fc248b3b0e165395345b05f08d8
Ryan Manchester <ryanm17@gmail.com>
2018-06-30 17:31:01 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 require 'sinatra/base'
-
+require 'pry'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
+      binding.pry
       erb :super_hero
     end
 
